d407da903aa767f966743928897ee257b76d209d
bonniielll <bonizmat1@gmail.com>
2024-09-06 18:15:39 +0500

    some fixes and edits

--- app/modules/router.py
+++ app/modules/router.py
@@ -21,7 +21,9 @@ async def clients_base(request: Request):
     clients_data = await ClientsDAO.find_all()
     keys = BaseModels.Clients.__table__.columns.keys()
     data = list()
-    column_names = ['Айди', 'Номер телефона', 'ФИО', 'Комментарий', 'Количество взаимодействий', 'Сумма прибыли', 'Сумма выплат', 'Первое взаимодействие', 'Последнее взаимодействие',]
+    column_names = ['Айди', 'Номер телефона', 'ФИО', 'Комментарий', 
+                    'Количество взаимодействий', 'Сумма прибыли', 'Сумма выплат', 
+                    'Первое взаимодействие', 'Последнее взаимодействие',]
     for n, i in enumerate(clients_data):
         columns = list()
         for row in keys:
@@ -38,34 +40,107 @@ async def clients_base(request: Request):
 
 @router.get('/newt')
 async def new_trading(request: Request):
-    newtrading_data = await NewTradingDAO.find_all()
+    trades_data = await NewTradingDAO.find_all()
     keys = BaseModels.NewTrading.__table__.columns.keys()
     data = list()
-    for n, i in enumerate(newtrading_data):
-        columns = dict()
+    column_names = ['Айди', 'Номер телефона', 'АКБ', 'Старый акб', 
+                    'Цена старого', 'Скидка', 'Сумма', 'Способ оплаты', 
+                    'Комментарий', 'По счету', 'Данные счета'
+                    , 'Дата создания', 'Дата обновления'] 
+    for n, i in enumerate(trades_data):
+        columns = list()
         for row in keys:
-            columns.update({row: getattr(newtrading_data[n], row)})
+            date = getattr(trades_data[n], row)
+            try:
+                date = date.strftime('%d.%m.%Y %Hч:%Mм')
+            except:
+                columns.append(getattr(trades_data[n], row))
+            else:
+                columns.append(date)
         data.append(columns)
-    print(data)
-    return templates.TemplateResponse(request=request, name='trading.html', context={"data": data})
+    return templates.TemplateResponse(request=request, name='trading.html', context={"data": data, "column_names": column_names})
 
 
 @router.get('/comt')
 async def comission_trading(request: Request):
-    return templates.TemplateResponse(request=request, name='trading.html', context={})
+    comission_data = await ComissionDAO.find_all()
+    keys = BaseModels.ComissionTrading.__table__.columns.keys()
+    data = list()
+    column_names = ['Айди', 'АКБ(название,код)', 'Гарантия(Мес)', 'Цена', 'Скидка',
+                     'Сумма', 'Метод оплаты', 'Клиент(номер)', 'Дата создания', 'Дата обновления']
+    for n, i in enumerate(comission_data):
+        columns = list()
+        for row in keys:
+            date = getattr(comission_data[n], row)
+            try:
+                date = date.strftime('%d.%m.%Y %Hч:%Mм')
+            except:
+                columns.append(getattr(comission_data[n], row))
+            else:
+                columns.append(date)
+        data.append(columns)
+    return templates.TemplateResponse(request=request, name='trading.html', context={"data": data, "column_names": column_names})
 
 
 @router.get('/scraptrade')
 async def scrap_trading(request: Request):
-    return templates.TemplateResponse(request=request, name='trading.html', context={})
+    scrap_data = await ScrapDAO.find_all()
+    keys = BaseModels.ScrapTrading.__table__.columns.keys()
+    data = list()
+    column_names = ['Айди', 'Клиент(номер телефона)', 'Объём АКБ', 'Сумма выплаты', 'Способ оплаты', 
+                    'Комментарий', 'Данные для мандарина', 'Ссылка на фото паспорта', 'Дата создания', 'Дата обновления']
+    for n, i in enumerate(scrap_data):
+        columns = list()
+        for row in keys:
+            date = getattr(scrap_data[n], row)
+            try:
+                date = date.strftime('%d.%m.%Y %Hч:%Mм')
+            except:
+                columns.append(getattr(scrap_data[n], row))
+            else:
+                columns.append(date)
+        data.append(columns)
+    return templates.TemplateResponse(request=request, name='trading.html', context={"data": data, "column_names": column_names})
+
 
 
 @router.get('/expertise')
 async def expertise(request: Request):
-    return templates.TemplateResponse(request=request, name='trading.html', context={})
+    expertise_data = await ExpertiseDAO.find_all()
+    keys = BaseModels.Expertise.__table__.columns.keys()
+    data = list()
+    column_names = ['Айди', 'Название акб', 'Клиент(номер)', 'Когда обнаружили брак', 'Где документы', 'Где акб', 
+                    'Комментарий', 'Подменный АКБ', 'Кто принял(имя)', 'Где приняли(точка)', 'Дата создания', 'Дата обновления']
+    for n, i in enumerate(expertise_data):
+        columns = list()
+        for row in keys:
+            date = getattr(expertise_data[n], row)
+            try:
+                date = date.strftime('%d.%m.%Y %Hч:%Mм')
+            except:
+                columns.append(getattr(expertise_data[n], row))
+            else:
+                columns.append(date)
+        data.append(columns)
+    return templates.TemplateResponse(request=request, name='trading.html', context={"data": data, "column_names": column_names})
 
 
 @router.get('/service')
 async def service(request: Request):
-    return templates.TemplateResponse(request=request, name='trading.html', context={})
-
+    scrap_data = await ServiceDAO.find_all()
+    keys = BaseModels.Service.__table__.columns.keys()
+    data = list()
+    column_names = ['Айди', 'Название АКБ', 'За услуги', 'Комментарий', 'Подменный АКБ', 
+                    'Залог за подмен', 'После возврата клиенту', 'Способ оплаты', 'Дата создания', 'Дата обновления']
+    for n, i in enumerate(scrap_data):
+        columns = list()
+        for row in keys:
+            date = getattr(scrap_data[n], row)
+            try:
+                date = date.strftime('%d.%m.%Y %Hч:%Mм')
+            except:
+                columns.append(getattr(scrap_data[n], row))
+            else:
+                columns.append(date)
+        data.append(columns)
+    return templates.TemplateResponse(request=request, name='trading.html', context={"data": data, "column_names": column_names})
